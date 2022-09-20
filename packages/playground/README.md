1283341e5fb091fdaafe91d1a94a696394d72375
jucian0 <juciano@outlook.com.br>
2022-09-20 20:55:48 -0300

    init

---
+++ packages/playground/README.md
@@ -0,0 +1,11 @@
+# playground
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test playground` to execute the unit tests via [Jest](https://jestjs.io).
+
+## Running lint
+
+Run `nx lint playground` to execute the lint via [ESLint](https://eslint.org/).
---
+++ packages/playground/src/playground.ts
@@ -0,0 +1,53 @@
+import flatten from 'lodash/flatten';
+import { toString } from 'hast-util-to-string';
+import { formatter } from './utils/format';
+import { componentName, sanitizeCode, removeTags } from './utils/jsx';
+import { getImportsVariables } from './utils/imports';
+import { getExportsVariables } from './utils/exports';
+
+const isPlayground = (name: string) => {
+  return name === 'Playground';
+};
+
+const addComponentsProps = (scopes: string[]) => (node: any, idx: number) => {
+  const name = componentName(node.value);
+  console.log(node);
+  const tagOpen = new RegExp(`^\\<${name}`);
+  const formatted = formatter(toString(node));
+  const code = formatted.slice(1, Infinity);
+  const scope = `{props, ${scopes.join(',')}}`;
+  const child = sanitizeCode(removeTags(code));
+  const newTag = `<${name} __position={${idx}} code={'${child}'} scope={${scope}}`;
+  node.value = node.value.replace(tagOpen, newTag);
+};
+
+export interface PluginOpts {
+  root: string;
+}
+
+export const injectCodeToPlayground =
+  () => (tree: any, file: { contents: string }) => {
+    const playgroundComponents: Node[] = tree.children.filter(
+      (node: any) => node.type === 'jsx'
+    );
+    // .filter((node) => {
+    // const name = componentName(node.value);
+    // return isPlayground(name);
+    // });
+
+    const importNodes = tree.children.filter((n: any) => n.type === 'import');
+    const exportNodes = tree.children.filter((n: any) => n.type === 'export');
+    const importedScopes = flatten<string>(
+      importNodes.map(getImportsVariables)
+    );
+    const exportedScopes = flatten<string>(
+      exportNodes.map(getExportsVariables)
+    ); // TODO exports not working, migrate to es lexer
+    // filter added to avoid throwing if an unexpected type is exported
+    const scopes: string[] = [...importedScopes, ...exportedScopes].filter(
+      Boolean
+    );
+    playgroundComponents.forEach(addComponentsProps(scopes));
+
+    return tree;
+  };
