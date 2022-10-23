e4cebff38f455e03f187b3d230bba6b604b4ba6f
jucian0 <juciano@outlook.com.br>
2022-10-23 16:49:09 -0300

    change docs build folder

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,10 +1,9 @@
 require('fix-esm').register();
-const { withNx } = require('@nrwl/next/plugins/with-nx');
 const { engrafia } = require('../dist/packages/engrafia-plugin');
 const compose = require('compose-function');
 const withImages = require('next-images');
 
-const composed = compose(engrafia, withNx, withImages);
+const composed = compose(engrafia,  withImages);
 
 module.exports = composed({
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
