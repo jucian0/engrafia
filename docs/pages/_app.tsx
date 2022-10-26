46700711c8e547bba2effdb23b5908f6f1b99e26
jucian0 <juciano@outlook.com.br>
2022-10-25 23:48:35 -0300

    remove analytics

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -3,7 +3,6 @@ import Head from 'next/head';
 import { NextUIProvider } from '@nextui-org/react';
 import { ThemeProvider } from 'next-themes';
 import { darkTheme, lightTheme, Provider } from '@engrafia/ui';
-import { Analytics } from '@vercel/analytics/react';
 
 function App({ Component, pageProps }: AppProps) {
   return (
@@ -18,7 +17,6 @@ function App({ Component, pageProps }: AppProps) {
       >
         <Provider>
           <Component {...pageProps} />
-          <Analytics />
         </Provider>
       </ThemeProvider>
     </NextUIProvider>
