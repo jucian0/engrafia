eb08175798367f637bdd2693883237883ab98078
jucian0 <juciano@outlook.com.br>
2022-11-03 22:17:01 -0300

    docs(homepage): improve homepage styles

--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -1,6 +1,9 @@
-import { Button, Grid, Text } from '@nextui-org/react';
+import { Button, Card, Grid, Link, Text } from '@nextui-org/react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import { CgPerformance } from 'react-icons/cg';
+import { IoRocketOutline } from 'react-icons/io5';
+import { BsHandThumbsUp } from 'react-icons/bs';
 import { useTranslation } from 'packages/engrafia-ui/src/useTranslation';
 
 export default function Index() {
@@ -15,6 +18,15 @@ export default function Index() {
           content="The easiest way to write docs for your project."
         />
       </Head>
+      <img
+        src="/imgs/background.svg"
+        style={{
+          position: 'absolute',
+          maxWidth: '60%',
+          height: 700,
+          right: 0,
+        }}
+      />
       <Grid.Container
         justify="center"
         direction="column"
@@ -35,7 +47,7 @@ export default function Index() {
         <Text>{t('description')}</Text>
       </Grid.Container>
 
-      <Grid.Container gap={2} justify="center">
+      <Grid.Container gap={2} justify="center" css={{ zIndex: 1 }}>
         <Grid>
           <Button
             onClick={() =>
@@ -47,41 +59,98 @@ export default function Index() {
             {t('get.start')}
           </Button>
         </Grid>
-        <Grid>
-          <Button
-            bordered
-            onClick={() =>
-              router.push(
-                `/docs/0.1.0/introduction/quick-start.${router.locale}`
-              )
-            }
-          >
-            {t('read.docs')}
-          </Button>
-        </Grid>
       </Grid.Container>
       <Grid.Container
         justify="center"
         direction="column"
         alignItems="center"
-        css={{ marginTop: '3rem' }}
+        css={{ marginTop: '3rem', marginBottom: '3rem', zIndex: 2 }}
       >
         <Text size="$2xl" color="$accents8">
           {t('sentence.brand')}
         </Text>
       </Grid.Container>
-      <Grid.Container gap={2} justify="center">
-        <Grid css={{ width: '20rem', height: '15rem', m: '3rem' }}>
-          <Text size="2rem">{t('performant')}</Text>
-          <Text color="$accents8">{t('performant.sentence')}</Text>
+
+      <Grid.Container gap={2} justify="center" css={{ zIndex: 1 }}>
+        <Grid>
+          <Card
+            css={{
+              p: '$6',
+              mw: '400px',
+              h: 250,
+              shadow: '$sm',
+
+              backdropFilter: 'saturate(180%) blur(14px)',
+              background: 'rgba(255, 255, 255, 0.05)',
+            }}
+          >
+            <Card.Header>
+              <CgPerformance size={40} />
+              <Grid.Container css={{ pl: '$6' }}>
+                <Grid xs={12}>
+                  <Text h4 css={{ lineHeight: '$xs' }}>
+                    {t('performant')}
+                  </Text>
+                </Grid>
+              </Grid.Container>
+            </Card.Header>
+            <Card.Body css={{ py: '$2' }}>
+              <Text>{t('performant.sentence')} </Text>
+            </Card.Body>
+          </Card>
         </Grid>
-        <Grid css={{ width: '20rem', height: '15rem', m: '3rem' }}>
-          <Text size="2rem">{t('next.power')}</Text>
-          <Text color="$accents8">{t('easy.to.use.sentence')}</Text>
+        <Grid>
+          <Card
+            css={{
+              p: '$6',
+              mw: '400px',
+              h: 250,
+              shadow: '$sm',
+
+              backdropFilter: 'saturate(180%) blur(14px)',
+              background: 'rgba(255, 255, 255, 0.05)',
+            }}
+          >
+            <Card.Header>
+              <IoRocketOutline size={40} />
+              <Grid.Container css={{ pl: '$6' }}>
+                <Grid xs={12}>
+                  <Text h4 css={{ lineHeight: '$xs' }}>
+                    {t('next.power')}
+                  </Text>
+                </Grid>
+              </Grid.Container>
+            </Card.Header>
+            <Card.Body css={{ py: '$2' }}>
+              <Text>{t('easy.to.use.sentence')} </Text>
+            </Card.Body>
+          </Card>
         </Grid>
-        <Grid css={{ width: '20rem', height: '15rem', m: '3rem' }}>
-          <Text size="2rem">{t('easy.to.use')}</Text>
-          <Text color="$accents8">{t('easy.to.use.sentence')}</Text>
+        <Grid>
+          <Card
+            css={{
+              p: '$6',
+              mw: '400px',
+              h: 250,
+              shadow: '$sm',
+              backdropFilter: 'saturate(180%) blur(14px)',
+              background: 'rgba(255, 255, 255, 0.05)',
+            }}
+          >
+            <Card.Header>
+              <BsHandThumbsUp size={40} />
+              <Grid.Container css={{ pl: '$6' }}>
+                <Grid xs={12}>
+                  <Text h4 css={{ lineHeight: '$xs' }}>
+                    {t('easy.to.use')}
+                  </Text>
+                </Grid>
+              </Grid.Container>
+            </Card.Header>
+            <Card.Body css={{ py: '$2' }}>
+              <Text>{t('easy.to.use.sentence')} </Text>
+            </Card.Body>
+          </Card>
         </Grid>
       </Grid.Container>
     </>
