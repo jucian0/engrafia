a12f8af2d8ba7fdd79b45bcbc3687013e1b6fc8a
jucian0 <juciano@outlook.com.br>
2022-11-08 21:05:50 -0300

    fix(engrafia-ui): fix Link component to work with i18n

--- packages/engrafia-ui/src/Components/Link.tsx
+++ packages/engrafia-ui/src/Components/Link.tsx
@@ -4,5 +4,9 @@ import { useRouter } from 'next/router';
 export function Link(props: L.LinkProps) {
   const { locale } = useRouter();
   const href = props.href;
-  return <L.default {...props} href={href.toString().concat(locale ?? '')} />;
+
+  if (locale) {
+    return <L.default {...props} href={href.toString().concat(locale)} />;
+  }
+  return <L.default {...props} href={href} />;
 }
