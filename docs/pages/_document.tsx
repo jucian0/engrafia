5aaceb79cb7e7f873a634fb7825daaa04ae57642
jucian0 <juciano@outlook.com.br>
2022-11-22 21:52:22 -0300

    docs(docs): update examples

--- docs/pages/_document.tsx
+++ docs/pages/_document.tsx
@@ -1,30 +1,19 @@
-import React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { CssBaseline } from '@nextui-org/react';
+import { Html, Head, Main, NextScript } from 'next/document';
 import { GlobalStyles } from 'engrafia';
+import { CssBaseline } from '@nextui-org/react';
 
-export default class CustomDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return {
-      ...initialProps,
-      styles: React.Children.toArray([initialProps.styles]),
-    };
-  }
-
-  render() {
-    return (
-      <Html>
-        <Head>
-          {CssBaseline.flush()}
-          {GlobalStyles()}
-          <link href="https://css.gg/link.css" rel="stylesheet" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {CssBaseline.flush()}
+        {GlobalStyles()}
+        <link href="https://css.gg/link.css" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
