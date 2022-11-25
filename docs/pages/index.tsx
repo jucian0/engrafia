47c153dca913ecf917f7969f85f99e4126a9e158
jucian0 <juciano@outlook.com.br>
2022-11-25 11:27:35 -0300

    docs(website): fix homepage style

--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -59,211 +59,214 @@ export default function Index() {
           right: 0,
         }}
       />
-      <Grid.Container
-        justify="center"
-        direction="column"
-        alignItems="center"
-        css={{ maxW: '90rem' }}
-      >
-        <Text
-          h1
-          css={{
-            textGradient: '45deg, $blue600 -20%, $pink600 50%',
-          }}
-          weight="bold"
-          size="5rem"
+      <Grid css={{ maxW: '90rem' }}>
+        <Grid.Container
+          justify="center"
+          direction="column"
+          alignItems="center"
+          css={{ maxW: '100%' }}
         >
-          Engrafia
-        </Text>
-
-        <Text as="h2" css={{ zIndex: '$1' }}>
-          {t('description')}
-        </Text>
-      </Grid.Container>
-
-      <Grid.Container
-        gap={2}
-        justify="center"
-        css={{ zIndex: 1, maxW: '90rem' }}
-      >
-        <Grid>
-          <Button
-            css={{ fontSize: '$lg' }}
-            onClick={() =>
-              router.push(
-                `/docs/0.1.0/introduction/quick-start.${router.locale}`
-              )
-            }
-          >
-            {t('get.start')}
-          </Button>
-        </Grid>
-      </Grid.Container>
-      <Grid.Container
-        justify="center"
-        direction="column"
-        alignItems="center"
-        css={{
-          marginTop: '3rem',
-          marginBottom: '3rem',
-          zIndex: 2,
-          maxW: '90rem',
-        }}
-      >
-        <Text size="$2xl" color="$accents8" css={{ zIndex: '$1' }}>
-          {t('sentence.brand')}
-        </Text>
-      </Grid.Container>
-
-      <Grid.Container
-        gap={2}
-        justify="center"
-        css={{ zIndex: 1, maxW: '90rem' }}
-      >
-        <Grid>
-          <Card
-            css={{
-              p: '$6',
-              mw: '400px',
-              h: 250,
-              shadow: '$sm',
-
-              backdropFilter: 'saturate(180%) blur(14px)',
-              background: 'rgba(255, 255, 255, 0.05)',
-            }}
-          >
-            <Card.Header>
-              <CgPerformance size={40} />
-              <Grid.Container css={{ pl: '$6' }}>
-                <Grid xs={12}>
-                  <Text h4 css={{ lineHeight: '$xs' }}>
-                    {t('performant')}
-                  </Text>
-                </Grid>
-              </Grid.Container>
-            </Card.Header>
-            <Card.Body css={{ py: '$2' }}>
-              <Text>{t('performant.sentence')} </Text>
-            </Card.Body>
-          </Card>
-        </Grid>
-        <Grid>
-          <Card
-            css={{
-              p: '$6',
-              mw: '400px',
-              h: 250,
-              shadow: '$sm',
-
-              backdropFilter: 'saturate(180%) blur(14px)',
-              background: 'rgba(255, 255, 255, 0.05)',
-            }}
-          >
-            <Card.Header>
-              <IoRocketOutline size={40} />
-              <Grid.Container css={{ pl: '$6' }}>
-                <Grid xs={12}>
-                  <Text h4 css={{ lineHeight: '$xs' }}>
-                    {t('next.power')}
-                  </Text>
-                </Grid>
-              </Grid.Container>
-            </Card.Header>
-            <Card.Body css={{ py: '$2' }}>
-              <Text>{t('easy.to.use.sentence')} </Text>
-            </Card.Body>
-          </Card>
-        </Grid>
-        <Grid>
-          <Card
-            css={{
-              p: '$6',
-              mw: '400px',
-              h: 250,
-              shadow: '$sm',
-              backdropFilter: 'saturate(180%) blur(14px)',
-              background: 'rgba(255, 255, 255, 0.05)',
-            }}
-          >
-            <Card.Header>
-              <BsHandThumbsUp size={40} />
-              <Grid.Container css={{ pl: '$6' }}>
-                <Grid xs={12}>
-                  <Text h4 css={{ lineHeight: '$xs' }}>
-                    {t('easy.to.use')}
-                  </Text>
-                </Grid>
-              </Grid.Container>
-            </Card.Header>
-            <Card.Body css={{ py: '$2' }}>
-              <Text>{t('easy.to.use.sentence')} </Text>
-            </Card.Body>
-          </Card>
-        </Grid>
-      </Grid.Container>
-
-      <Grid css={{ borderTop: '1px solid $border', w: '80rem', my: '5rem' }} />
-
-      <Grid.Container
-        gap={2}
-        justify="center"
-        css={{ zIndex: 1, mb: 100, maxW: '80rem' }}
-      >
-        <Grid css={{ w: '50%' }} justify="flex-end">
-          <Text h1 css={{ lineHeight: '$xs' }}>
-            {'Metadata, '}
-            <Text color="primary" span css={{ lineHeight: '$xs' }}>
-              SEO
-            </Text>
-            {', sidebar organization'}
-          </Text>
-          <Text>
-            {
-              'Organize your content, improve SEO, and search engine results, and organize your content in the sidebar'
-            }
-            <Badge isSquared color="secondary" variant="flat">
-              <Link href="/docs/0.1.0/features/seo">Read more</Link>
-            </Badge>
-          </Text>
-        </Grid>
-        <Grid css={{ w: '50%' }}>
-          <Code className="markdown" boxShadow="$md">
-            {code}
-          </Code>
-        </Grid>
-      </Grid.Container>
-
-      <Grid.Container gap={2} css={{ zIndex: 1, maxW: '80rem', mb: '8rem' }}>
-        <Grid css={{ w: '100%' }} justify="flex-end">
           <Text
             h1
             css={{
-              lineHeight: '$xs',
               textGradient: '45deg, $blue600 -20%, $pink600 50%',
             }}
+            weight="bold"
+            size="5rem"
           >
-            Code highlight{' '}
+            Engrafia
           </Text>
-          <Text>
-            {
-              'Powered by PrismJs, write, and show beautiful code highlight, and organize it easily'
-            }
-            <Badge isSquared color="secondary" variant="flat">
-              <Link href="/docs/0.1.0/features/code-highlight">Read more</Link>
-            </Badge>
+
+          <Text as="h2" css={{ zIndex: '$1' }}>
+            {t('description')}
           </Text>
-        </Grid>
-        <Grid css={{ w: '50%' }}>
-          <Code className="javascript" boxShadow="$md">
-            {jsCode}
-          </Code>
-        </Grid>
-        <Grid css={{ w: '50%' }}>
-          <Code className="python" boxShadow="$md">
-            {pyCode}
-          </Code>
-        </Grid>
-      </Grid.Container>
+        </Grid.Container>
+
+        <Grid.Container
+          gap={2}
+          justify="center"
+          css={{ zIndex: 1, maxW: '100%' }}
+        >
+          <Grid>
+            <Button
+              css={{ fontSize: '$lg' }}
+              onClick={() =>
+                router.push(
+                  `/docs/0.1.0/introduction/quick-start.${router.locale}`
+                )
+              }
+            >
+              {t('get.start')}
+            </Button>
+          </Grid>
+        </Grid.Container>
+        <Grid.Container
+          justify="center"
+          direction="column"
+          alignItems="center"
+          css={{
+            marginTop: '3rem',
+            marginBottom: '3rem',
+            zIndex: 2,
+            maxW: '100%',
+          }}
+        >
+          <Text size="$2xl" color="$accents8" css={{ zIndex: '$1' }}>
+            {t('sentence.brand')}
+          </Text>
+        </Grid.Container>
+
+        <Grid.Container
+          gap={2}
+          justify="center"
+          css={{ zIndex: 1, maxW: '100%' }}
+        >
+          <Grid>
+            <Card
+              css={{
+                p: '$6',
+                mw: '400px',
+                h: 250,
+                shadow: '$sm',
+
+                backdropFilter: 'saturate(180%) blur(14px)',
+                background: 'rgba(255, 255, 255, 0.05)',
+              }}
+            >
+              <Card.Header>
+                <CgPerformance size={40} />
+                <Grid.Container css={{ pl: '$6' }}>
+                  <Grid xs={12}>
+                    <Text h4 css={{ lineHeight: '$xs' }}>
+                      {t('performant')}
+                    </Text>
+                  </Grid>
+                </Grid.Container>
+              </Card.Header>
+              <Card.Body css={{ py: '$2' }}>
+                <Text>{t('performant.sentence')} </Text>
+              </Card.Body>
+            </Card>
+          </Grid>
+          <Grid>
+            <Card
+              css={{
+                p: '$6',
+                mw: '400px',
+                h: 250,
+                shadow: '$sm',
+
+                backdropFilter: 'saturate(180%) blur(14px)',
+                background: 'rgba(255, 255, 255, 0.05)',
+              }}
+            >
+              <Card.Header>
+                <IoRocketOutline size={40} />
+                <Grid.Container css={{ pl: '$6' }}>
+                  <Grid xs={12}>
+                    <Text h4 css={{ lineHeight: '$xs' }}>
+                      {t('next.power')}
+                    </Text>
+                  </Grid>
+                </Grid.Container>
+              </Card.Header>
+              <Card.Body css={{ py: '$2' }}>
+                <Text>{t('easy.to.use.sentence')} </Text>
+              </Card.Body>
+            </Card>
+          </Grid>
+          <Grid>
+            <Card
+              css={{
+                p: '$6',
+                mw: '400px',
+                h: 250,
+                shadow: '$sm',
+                backdropFilter: 'saturate(180%) blur(14px)',
+                background: 'rgba(255, 255, 255, 0.05)',
+              }}
+            >
+              <Card.Header>
+                <BsHandThumbsUp size={40} />
+                <Grid.Container css={{ pl: '$6' }}>
+                  <Grid xs={12}>
+                    <Text h4 css={{ lineHeight: '$xs' }}>
+                      {t('easy.to.use')}
+                    </Text>
+                  </Grid>
+                </Grid.Container>
+              </Card.Header>
+              <Card.Body css={{ py: '$2' }}>
+                <Text>{t('easy.to.use.sentence')} </Text>
+              </Card.Body>
+            </Card>
+          </Grid>
+        </Grid.Container>
+
+        <Grid css={{ borderTop: '1px solid $border', w: '100%', my: '5rem' }} />
+
+        <Grid.Container
+          gap={2}
+          justify="center"
+          css={{ zIndex: 1, mb: 100, maxW: '100%' }}
+        >
+          <Grid css={{ w: '50%' }} justify="flex-end">
+            <Text h1 css={{ lineHeight: '$xs' }}>
+              {'Metadata, '}
+              <Text color="primary" span css={{ lineHeight: '$xs' }}>
+                SEO
+              </Text>
+              {', sidebar organization'}
+            </Text>
+            <Text>
+              {
+                'Organize your content, improve SEO, and search engine results, and organize your content in the sidebar'
+              }
+              <Badge isSquared color="secondary" variant="flat">
+                <Link href="/docs/0.1.0/features/seo">Read more</Link>
+              </Badge>
+            </Text>
+          </Grid>
+          <Grid css={{ w: '50%' }}>
+            <Code className="markdown" boxShadow="$md">
+              {code}
+            </Code>
+          </Grid>
+        </Grid.Container>
+
+        <Grid.Container gap={2} css={{ zIndex: 1, maxW: '100%', mb: '8rem' }}>
+          <Grid css={{ w: '100%' }} justify="flex-end">
+            <Text
+              h1
+              css={{
+                textGradient: '45deg, $blue600 -20%, $pink600 50%',
+              }}
+            >
+              Code highlight{' '}
+            </Text>
+            <Text>
+              {
+                'Powered by PrismJs, write, and show beautiful code highlight, and organize it easily'
+              }
+              <Badge isSquared color="secondary" variant="flat">
+                <Link href="/docs/0.1.0/features/code-highlight">
+                  Read more
+                </Link>
+              </Badge>
+            </Text>
+          </Grid>
+          <Grid css={{ w: '50%' }}>
+            <Code className="javascript" boxShadow="$md">
+              {jsCode}
+            </Code>
+          </Grid>
+          <Grid css={{ w: '50%' }}>
+            <Code className="python" boxShadow="$md">
+              {pyCode}
+            </Code>
+          </Grid>
+        </Grid.Container>
+      </Grid>
     </Grid.Container>
   );
 }
