b1b28d572bd2fb7e7999bd958b77102880f9c2f6
jucian0 <juciano@outlook.com.br>
2022-12-06 16:40:34 -0300

    docs(website): try to use tags in _app

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -8,26 +8,26 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <Engrafia>
       <Head>
-        <title>{`Engrafia | ${meta.title}`}</title>
-        <meta name="description" content={meta.description} />
+        <title>{`Engrafia | ${meta?.title}`}</title>
+        <meta name="description" content={meta?.description} />
 
         {/* <!-- Facebook Meta Tags --> */}
         <meta property="og:url" content="https://engrafia.vercel.app/" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content={`Engrafia | ${meta.title}`} />
-        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={`Engrafia | ${meta?.title}`} />
+        <meta property="og:description" content={meta?.description} />
         <meta property="og:image" content="/imgs/engrafia.jpg" />
 
         {/* <!-- Twitter Meta Tags --> */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="engrafia.vercel.app" />
         <meta property="twitter:url" content="https://engrafia.vercel.app/" />
-        <meta name="twitter:title" content={`Engrafia | ${meta.title}`} />
-        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:title" content={`Engrafia | ${meta?.title}`} />
+        <meta name="twitter:description" content={meta?.description} />
         <meta name="twitter:image" content="/imgs/engrafia.jpg" />
 
         <link rel="sitemap" href="/sitemap.xml" />
-        <meta name="keywords" content={meta.tags} />
+        <meta name="keywords" content={meta?.tags} />
         <link rel="shortcut icon" href="/favicon/favicon.ico" />
         <link
           rel="apple-touch-icon"
